2c7e5d3ab13d07f377217498116d8136d225adff
Robbie <robbie331@>
2024-04-30 10:16:53 +0900

    feat: Add operator input feature

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int firstNum, secondNum;
+        char operator;
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
@@ -14,5 +15,8 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         secondNum = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        operator = sc.next().charAt(0);
     }
 }
